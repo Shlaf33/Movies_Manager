84ced50e25597497cb67c817635229565b4c6c80
Adrien Heral <adrienheral@hotmail.com>
2025-02-14 11:02:02 +0100

    Adding User and Movies attributes, creating first step of IHM

--- app/src/main/java/com/example/movies_manager/model/Movie.java
+++ app/src/main/java/com/example/movies_manager/model/Movie.java
@@ -1,4 +1,67 @@
 package com.example.movies_manager.model;
 
 public class Movie {
+
+    private int id_title;
+    private String overview, popularity, posterPath;
+    private boolean isFavorite;
+
+
+    //***********************
+    //Constructor
+    //***********************
+
+    public Movie(int id_title, String overview, String popularity, String posterPath, boolean isFavorite) {
+        this.id_title = id_title;
+        this.overview = overview;
+        this.popularity = popularity;
+        this.posterPath = posterPath;
+        this.isFavorite = isFavorite;
+    }
+
+
+
+    //***********************
+    //Getter and Setter
+    //***********************
+
+    public int getId_title() {
+        return id_title;
+    }
+
+    public void setId_title(int id_title) {
+        this.id_title = id_title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(String popularity) {
+        this.popularity = popularity;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public boolean isFavorite() {
+        return isFavorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        isFavorite = favorite;
+    }
 }
--- app/src/main/java/com/example/movies_manager/model/User.java
+++ app/src/main/java/com/example/movies_manager/model/User.java
@@ -1,4 +1,67 @@
 package com.example.movies_manager.model;
 
+import java.util.Date;
+
 public class User {
+
+    private int id, session_id, guest_session_id;
+    private String username;
+    private Date expire_at;
+
+    //********************
+    //Constructor
+    //********************
+
+    public User(int id, int session_id, int guest_session_id, String username, Date expire_at) {
+        this.id = id;
+        this.session_id = session_id;
+        this.guest_session_id = guest_session_id;
+        this.username = username;
+        this.expire_at = expire_at;
+    }
+
+    //********************
+    //Getter and Setter
+    //********************
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSession_id() {
+        return session_id;
+    }
+
+    public void setSession_id(int session_id) {
+        this.session_id = session_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getGuest_session_id() {
+        return guest_session_id;
+    }
+
+    public void setGuest_session_id(int guest_session_id) {
+        this.guest_session_id = guest_session_id;
+    }
+
+    public Date getExpire_at() {
+        return expire_at;
+    }
+
+    public void setExpire_at(Date expire_at) {
+        this.expire_at = expire_at;
+    }
 }
